bf8937693d3cd5ac3bea7457611b165c8ef7c465
Emmanuel <110588345+BigDoro@users.noreply.github.com>
2025-08-05 12:52:33 +0100

    IrisFlower_app.py

--- IrisFlower_app.py
+++ IrisFlower_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import scikit-learn as sklearn
 from sklearn.datasets import load_iris
 
 iris= load_iris()
@@ -38,6 +37,7 @@ if st.button('Predict'):
     st.info(f'Model confidence: Setosa: {probability_predict[0] * 100:.2f}%, '
             f'Versicolor: {probability_predict[1] * 100:.2f}%, '
             f'Virginica: {probability_predict[2] * 100:.2f}%')
+
 
 
 
